9c7c2237612c9ba93f7009e3ae449dfb984b93b3
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2022-04-17 16:33:16 +0900

    custom start

---
+++ app/src/main/java/kr/co/si312/breakingtime/utils/KeyboardEng.kt
@@ -0,0 +1,21 @@
+package kr.co.si312.breakingtime.utils
+
+import android.content.Context
+import android.view.View
+
+class KeyboardEng : View {
+    constructor(context: Context?): super(context)
+
+    companion object {
+        private const val ASPECT = 100f
+    }
+
+    init {
+
+    }
+
+    private fun initialize(){
+
+
+    }
+}
